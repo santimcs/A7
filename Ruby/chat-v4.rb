00f73b914a564a37b1ef3815051c72f3d5cbe949
santimcs <santimcs@hotmail.com>
2023-01-14 14:03:16 +0700

    23-01-14 02

--- Ruby/chat-v4.rb
+++ Ruby/chat-v4.rb
@@ -1,70 +1,67 @@
 require 'csv'
 
 def summarize_prices(input_file, output_file)
-    previous_name = nil
     previous_row = nil
-    previous_trend = 'First Record'
+    previous_trend = nil
     start_date = nil
     start_price = nil
     trend = nil
-    no = 0
+    nbr = 0
 
     CSV.open(output_file, 'w') do |output|
-        output << %w[name from_date to_date from_price to_price trend no]
-        str = %w[name from_date to_date from_price to_price trend no].join(',')
-        puts str
+        output << %w[name from_date to_date from_price to_price trend nbr]
+        header = %w[name from_date to_date from_price to_price trend nbr].join(',')
+        puts header
 
         CSV.foreach(input_file) do |row|
-            no = no + 1
+            nbr = nbr + 1
             if previous_row.nil? # process for 1st row
                 start_date = row[1]
                 start_price = row[2]
-                previous_name = row[0]  
             end
 
             if !previous_row.nil? # process for 2nd row onward
-                if row[0] != previous_name
-                    output << [previous_name, start_date, previous_row[1], start_price, previous_row[2], previous_trend, no]
-                    output_string = [previous_name, start_date, previous_row[1], start_price, previous_row[2], previous_trend, no].join(',')
+                if row[0] != previous_row[0]    # group break
+                    output << [previous_row[0], start_date, previous_row[1], start_price, previous_row[2], previous_trend, nbr]
+                    output_string = [previous_row[0], start_date, previous_row[1], start_price, previous_row[2], previous_trend, nbr].join(',')
                     puts output_string                    
                     start_date = row[1]
                     start_price = row[2]
-                    trend = 'First Record'
+                    trend = nil
                 end
 
-            end            
+                if row[0] == previous_row[0]    # same group
 
-            if !previous_row.nil? # process for 2nd row onward
-
-                if row[2] > previous_row[2]
-                    trend = "Upward"
-                end
-                if row[2] < previous_row[2]
-                    trend = "Downward"
-                end
-
-                if trend != previous_trend
-                    if row[0] == previous_name
-                        output << [previous_name, start_date, previous_row[1], start_price, previous_row[2], previous_trend, no]
-                        output_string = [previous_name, start_date, previous_row[1], start_price, previous_row[2], previous_trend, no].join(',')
-                        puts output_string
+                    if row[2] > previous_row[2]
+                        trend = "Upward"
+                    end
+                    if row[2] < previous_row[2]
+                        trend = "Downward"
                     end
 
-                    start_date = row[1]
-                    start_price = row[2]
-                    previous_trend = trend
+                    if trend != previous_trend
+
+                        if row[0] == previous_row[0]
+                            output << [previous_row[0], start_date, previous_row[1], start_price, previous_row[2], previous_trend, nbr]
+                            output_string = [previous_row[0], start_date, previous_row[1], start_price, previous_row[2], previous_trend, nbr].join(',')
+                            puts output_string
+                        end
+
+                        start_date = row[1]
+                        start_price = row[2]
+                        previous_trend = trend
+                    end
+
                 end
 
             end # end of process for 2nd row onward
 
             previous_row = row  # do every row
-            previous_name = row[0]  # do every row
         end # end of foreach row
 
         # process after last row
-        # previous_trend = 'Last Record'
-        output << [previous_name, start_date, previous_row[1], start_price, previous_row[2], previous_trend, no]
-        output_string = [previous_name, start_date, previous_row[1], start_price, previous_row[2], previous_trend, no].join(',')
+        output << [previous_row[0], start_date, previous_row[1], start_price, previous_row[2], previous_trend, nbr]
+        output_string = [previous_row[0], start_date, previous_row[1], start_price, previous_row[2], previous_trend, nbr].join(',')
         puts output_string
     
     end # end of output_file
@@ -91,5 +88,3 @@ summarize_prices(input_file, output_file)
 new_output_file = File.join(output_dir, "summary.csv")
 File.rename(output_file, new_output_file)
 puts new_output_file
-
-
